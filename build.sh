5f217c00b7fda74d74595f0b084e0483243e3135
alach <andrzej.lach@turbineanalytics.com>
2018-01-24 09:19:46 +0100

    TAMAPI-1916 remove unwanted images

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 
 set -euo pipefail
 
-docker build -t turbineanalytics/sonarqube:5.6.6 5.6.6
-docker build -t turbineanalytics/sonarqube:5.6.6-alpine 5.6.6-alpine
-docker build -t turbineanalytics/sonarqube:6.4 6.4
 docker build -t turbineanalytics/sonarqube:6.4-alpine 6.4-alpine
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
